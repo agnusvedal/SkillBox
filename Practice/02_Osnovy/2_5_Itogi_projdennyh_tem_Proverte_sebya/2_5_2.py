967f7c4f22d17cfcc9ff0edcf105e7269d18924a
agnusvedal <10660493+agnusvedal@users.noreply.github.com>
2025-02-11 01:13:27 +0300

    [add] добавлены задач до 3.3

---
+++ Practice/02_Osnovy/2_5_Itogi_projdennyh_tem_Proverte_sebya/2_5_2.py
@@ -0,0 +1,13 @@
+""" Задача 2. Цвета
+Исправьте программу так, чтобы в результате её выполнения на экран в одну строку выводился текст Red Blue Green RedGreenBlue Blue GreenBlue.
+r = 'Red'
+g = 'Green'
+b = 'Blue'
+print(b, r, g, b, g + b, b + b + g, b) """
+
+r = 'Red'
+g = 'Green'
+b = 'Blue'
+print(r, b, g, r + g + b, b, g + b)
+# Строка проверки:
+print('Red Blue Green RedGreenBlue Blue GreenBlue')
